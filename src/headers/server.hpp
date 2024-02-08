098747837a03dabfec2f63dc559adcc02bd4e98b
Van Antonietti <v.antonietti@icloud.com>
2024-02-08 16:13:39 +0000

    initial configuration header files

---
+++ src/headers/server.hpp
@@ -0,0 +1,31 @@
+
+#ifndef CONFIGURATION_HPP
+#define CONFIGURATION_HPP
+
+#include <iostream>
+#include <map>
+#include <stdexcept>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#define SLABEL "server"
+
+namespace Config {
+     struct Router {
+    };
+
+    typedef struct s_server {
+    }   Server;
+
+    class Configuration {
+    private:
+        std::vector<Server> _config;
+        Server _initServer();
+        
+    public:
+        Configuration();
+        ~Configuration();
+    };
+};
+#endif // CONFIGURATION_HPP
